4c1618234cd110734f709f6afdaae689bc5603f8
Glen Hughes <glen.866@gmail.com>
2019-02-20 18:59:13 +0000

    Match UI for not found page

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from './Header';
+
+const MessageBox = styled.div`
+  margin: 0 auto;
+  margin-top: 20px;
+  padding: 40px;
+  text-align: center;
+  background-color: paleVioletRed;
+  color: linen;
+  border-radius: 20px;
+  width: 400px;
+`;
 
 const NotFound = () => (
-  <div className="NotFound">
-    <p>Not found.</p>
+  <div>
+    <Header />
+    <div className="container">
+      <MessageBox>Page not found!</MessageBox>
+    </div>
   </div>
 );
 
