2e11498c0396b6d2b356810a91e0bcd67e038544
JamesBoadi <jamesboadi30@yahoo.co.uk>
2021-07-05 15:26:25 +0100

    Update Files

--- client/index.js
+++
@@ -1,9 +0,0 @@
-require('hpropagate')();
-
-const server = require('./server');
-const { PORT } = require('./env-vars');
-const logger = require('./logger');
-
-server.listen(PORT, () => {
-  logger.info('Server running on port %d', PORT);
-});
--- client/logger.js
+++
@@ -1,5 +0,0 @@
-const bunyan = require('bunyan');
-
-const logger = bunyan.createLogger({ name: 'code-test' });
-
-module.exports = logger;
